862d0f34b6eb24c641977de76f79af3fce47ec9d
Swaminathan <sayeeiyer@gmail.com>
2020-07-25 23:18:56 +0530

    Added Intro

---
+++ src/Swaminathan/LandingPage/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './LandingPage.module.css';
+
+function LandingPage() {
+    return (
+        <div className={styles.rootContainer}>
+            <div className={styles.rightSide}>
+                <div className={styles.box}>
+                    
+                        <div className={styles.nameRbn}>Eat</div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
